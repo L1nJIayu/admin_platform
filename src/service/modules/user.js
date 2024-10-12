c2fa219a1718a970e5e425f6bb0b5870bb8b9594
L1nJIayu <562316920@qq.com>
2024-10-12 17:01:34 +0800

    update UserTable

---
+++ src/service/modules/user.js
@@ -0,0 +1,20 @@
+import axios from '../axios'
+import { useId } from 'react'
+
+export function getTableDataApi(params) {
+  console.log('发起请求')
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        list: [
+          { id: 1, username: 'jeffrey', chineseName: '林嘉裕', status: 0, createAt: '2024-10-10 20:37:11' },
+          { id: 2, username: 'a', chineseName: '小A', status: 1, createAt: '2024-10-10 20:37:22' },
+          { id: 3, username: 'b', chineseName: '小B', status: 2, createAt: '2024-10-10 20:37:33' },
+          { id: 4, username: 'c', chineseName: '小C', status: 3, createAt: '2024-10-10 20:37:44' },
+        ],
+        total: 21
+      })
+    }, 1000)
+  })
+  return axios.post('/login', params)
+}
